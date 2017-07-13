72c60191f205335ef2617f8808d24798ce071c26
Alexander Stepanov <vaiol.ans@gmail.com>
2017-07-13 19:18:47 +0300

    ip.html test file started

---
+++ ip.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>IP</title>
+    <link rel="stylesheet" type="text/css" href="style/ip.css?v=<?=time();?>">
+</head>
+<body>
+<header>
+    <nav>
+
+    </nav>
+</header>
+<div id="content">
+    <div class="container">
+        <div class="photo">
+
+        </div>
+        <div class="photo">
+
+        </div>
+        <div class="photo">
+
+        </div>
+        <div class="photo">
+
+        </div>
+        <div class="photo">
+
+        </div>
+        <div class="photo">
+
+        </div>
+        <div class="photo">
+
+        </div>
+        <div class="photo">
+
+        </div>
+        <div class="photo">
+
+        </div>
+        <div class="photo">
+
+        </div>
+        <div class="photo">
+
+        </div>
+        <div class="photo">
+
+        </div>
+        <div class="photo">
+
+        </div>
+        <div class="photo">
+
+        </div>
+        <div class="photo">
+
+        </div>
+        <div class="photo">
+
+        </div>
+        <div class="photo">
+
+        </div>
+        <div class="photo">
+
+        </div>
+        <div class="photo">
+
+        </div>
+        <div class="photo">
+
+        </div>
+        <div class="photo">
+
+        </div>
+        <div class="photo">
+
+        </div>
+        <div class="photo">
+
+        </div>
+        <div class="photo">
+
+        </div>
+    </div>
+</div>
+<footer>
+
+</footer>
+</body>
+</html>
