57952b80e6f8dc777d112ffeca60fb411f6b5023
Alexander Stepanov <astepano@e1r7p7.unit.ua>
2017-07-17 20:07:13 +0300

    sidebar closing animation

--- ip.php
+++ ip.php
@@ -8,17 +8,17 @@
 </head>
 <body class="Site">
 <header>
-    <button onclick="w3_open()" class="w3-button w3-large" id="open-btn">☰</button>
+    <button onclick="sidebarOpen()" id="open-btn">☰</button>
     <nav>
         <img id="logo" src="img/logo.png" alt="logo">
     </nav>
-    <div class="w3-bar-block" style="display:none;" id="mySidebar">
-        <button onclick="w3_close()" class="w3-bar-item w3-large">Close &times;</button>
-        <a href="#" class="w3-bar-item w3-button">Link 1</a>
-        <a href="#" class="w3-bar-item w3-button">Link 2</a>
-        <a href="#" class="w3-bar-item w3-button">Link 3</a>
+    <div id="mySidebar" class="hidden sidebarClose">
+        <button onclick="sidebarClose()">Close &times;</button>
+        <a href="#">Link 1</a>
+        <a href="#">Link 2</a>
+        <a href="#">Link 3</a>
     </div>
-    <div  id="myOverlay" onclick="w3_close()" style="cursor:pointer"></div>
+    <div id="myOverlay" class="hidden overlayClose" onclick="sidebarClose()"></div>
 </header>
 <div id="content">
     <div class="container">
--- js/ip.js
+++ js/ip.js
@@ -1,9 +1,25 @@
-function w3_open() {
-    document.getElementById("mySidebar").style.display = "block";
-    document.getElementById("myOverlay").style.display = "block";
+function sidebarOpen() {
+    var overlay = document.getElementById("myOverlay");
+    var sidebar = document.getElementById("mySidebar");
+    overlay.classList.remove('hidden');
+    sidebar.classList.remove('hidden');
+    overlay.style.display = "block";
+    sidebar.style.display = "block";
+    overlay.classList.toggle('overlayOpen');
+    overlay.classList.toggle('overlayClose');
+    sidebar.classList.toggle('sidebarOpen');
+    sidebar.classList.toggle('sidebarClose');
 }
 
-function w3_close() {
-    document.getElementById("mySidebar").style.display = "none";
-    document.getElementById("myOverlay").style.display = "none";
+function sidebarClose() {
+    var overlay = document.getElementById("myOverlay");
+    var sidebar = document.getElementById("mySidebar");
+    overlay.classList.toggle('overlayOpen');
+    overlay.classList.toggle('overlayClose');
+    sidebar.classList.toggle('sidebarOpen');
+    sidebar.classList.toggle('sidebarClose');
+    setTimeout(function() {
+        overlay.style.display = "none";
+        sidebar.style.display = "none";
+    }, 300);
 }
