bc00849818c99496301e50cc0c4b433d05e45085
Alexander Stepanov <vaiol.ans@gmail.com>
2017-09-13 19:24:30 +0300

    cam page completed

--- cam.php
+++ cam.php
@@ -6,15 +6,18 @@
             <div class="line"></div>
         </div>
     </div>
+    <video id="video" autoplay></video>
     <div id="cam">
         <div class="container">
-            <video id="video" autoplay></video>
-            <input type='file' id='getimage' name="background-image" onchange="uploadImg();this.value=null;return false;">
+            <form method="post" accept-charset="utf-8" name="form1">
+                <input name="hidden_data" id='hidden_data' type="hidden"/>
+            </form>
+            <input type='file' id='uploadImg' class="none" name="upImg" onchange="uploadImg();this.value=null;return false;">
+            <input type='file' id='uploadMask' class="none" name="upMask" onchange="uploadMask();this.value=null;return false;">
             <div id="overlay-canvas">
                 <p id="click-text">CLICK TO DOWNLOAD OTHER IMAGE</p>
                 <canvas id="canvas"></canvas>
             </div>
-
             <div id="previews">
                 <p>PREVIEW</p>
                 <canvas id="canvas1"></canvas>
@@ -22,39 +25,47 @@
                 <canvas id="canvas3"></canvas>
                 <canvas id="canvas4"></canvas>
             </div>
-            Rotate:
-            <input type="range" name="rotate" min="-90" max="90" id="rotat">
-            Resize:
-            <input type="range" name="resize" min="-400" max="400" id="resize">
             <input type="hidden" value="0" id="img_resize">
-            <div id="effects">
-                <div class="effect" onclick="changeEffect('img/effects/e1.png')">
-                    <img src="img/effects/e1.png" alt="effect1">
+            <div id="effects-block">
+                <div id="effects">
+                    <div class="effect" onclick="changeEffect('img/effects/e1.png')">
+                        <img src="img/effects/p1.png" alt="effect1">
+                    </div>
+                    <div class="effect" onclick="changeEffect('img/effects/e2.png')">
+                        <img src="img/effects/p2.png" alt="effect2">
+                    </div>
+                    <div class="effect" onclick="changeEffect('img/effects/e3.png')">
+                        <img src="img/effects/p3.png" alt="effect3">
+                    </div>
+                    <div class="effect" onclick="changeEffect('img/effects/e4.png')">
+                        <img src="img/effects/p4.png" alt="effect4">
+                    </div>
+                    <div class="effect" onclick="document.getElementById('uploadMask').click();">
+                        <p>UPLOAD YOUR MASK</p>
+                    </div>
                 </div>
-                <div class="effect" onclick="changeEffect('img/effects/e2.png')">
-                    <img src="img/effects/e2.png" alt="effect2">
-                </div>
-                <div class="effect" onclick="changeEffect('img/effects/e3.png')">
-                    <img src="img/effects/e3.png" alt="effect3">
-                </div>
-                <div class="effect" onclick="changeEffect('img/effects/e4.png')">
-                    <img src="img/effects/e4.png" alt="effect4">
+                <div id="eff-control">
+                    <p>ROTATE</p><p>RESIZE</p>
+                    <input type="range" name="rotate" min="-90" max="90" id="rotat" style="margin-right: .5%">
+                    <input type="range" name="resize" min="-70" max="70" id="resize" style="margin-left: .5%">
                 </div>
             </div>
+
 
         </div>
     </div>
     <div class="container">
         <div id="control">
-            <div class="butt" style="margin-right: 0.4%" onclick="clearEffect()">
+            <div class="butt"  onclick="clearEffect()">
                 CLEAR
             </div>
-            <div class="butt" id="snap" style="margin-left: 0.4%" onclick="saveImage()">
+            <div class="butt" id="uploadButt" onclick="document.getElementById('uploadImg').click();">
+                UPLOAD IMAGE
+            </div>
+            <div class="butt" id="snap"  onclick="saveImage()">
                 SAVE
             </div>
-            <form method="post" accept-charset="utf-8" name="form1">
-                <input name="hidden_data" id='hidden_data' type="hidden"/>
-            </form>
+
         </div>
     </div>
 </div>
@@ -62,4 +73,4 @@
 <script src="js/input-range-ui.js"></script>
 <?php require "footer.html"; ?>
 
-<!--                 onclick="document.getElementById('getimage').click();"-->
+<!--                 onclick="document.getElementById('uploadImg').click();"-->
--- js/cam.js
+++ js/cam.js
@@ -32,6 +32,10 @@ var prevX = 0;
 var prevY = 0;
 var uploadedImg = null;
 var maskImg = null;
+var maskFlag = false;
+var prevCanvasWidth = -1;
+
+
 
 function changePageStyle() {
     if (window.matchMedia( "(min-width: 601px)" ).matches) {
@@ -40,6 +44,7 @@ function changePageStyle() {
         document.getElementById('previews').style.height= 'auto';
     }
     document.getElementById('click-text').style.lineHeight = document.getElementById('canvas').offsetHeight+'px';
+
 }
 
 function renderFrame() {
@@ -57,18 +62,30 @@ function renderFrame() {
         context.drawImage(uploadedImg, 0, 0, canvas.width, canvas.height);
     }
 
-    if (maskImg !== null)
-    {
-        //RENDER MASK FRAME WITH ROTATING, RESIZING AND MOVING
+    if (maskFlag) {
+        //Change mask size to the canvas size:
+        const ratio = (maskImg.width / maskImg.height);
+        maskImg.height = canvas.height;
+        maskImg.width = maskImg.height * ratio;
+        //count shift to place mask in center of canvas:
+        var shiftX = canvas.width - maskImg.width;
+        shiftX = shiftX > 0 ? shiftX / 2 : 0;
+        //get size of mask after resize:
         const resize = parseInt(document.getElementById("resize").value);
-        const imgWidth = parseInt(canvas.width) + resize;
-        const imgHeight = parseInt(canvas.height) + resize;
-        const nextMoveX = moveX - resize / 2;
-        const nextMoveY = moveY - resize / 2;
+        const resizeX = maskImg.width * (resize / 100);
+        const resizeY = maskImg.height * (resize / 100);
+        const imgWidth = maskImg.width + resizeX;
+        const imgHeight = maskImg.height + resizeY;
+        //change moveXY if canvas.width was changed
+        const moveRatio = prevCanvasWidth > 0 ? prevCanvasWidth / canvas.width : 1;
+        //get new coordinate for mask:
+        const nextMoveX = shiftX + (moveX / moveRatio) - resizeX / 2;
+        const nextMoveY = (moveY / moveRatio) - resizeY / 4;
+        //draw mask:
         context.save();
         context.translate(nextMoveX + imgWidth / 2, nextMoveY + imgHeight / 3);
         context.rotate(document.getElementById("rotat").value * Math.PI / 180);
-        context.translate((nextMoveX + imgWidth /2) * -1, (nextMoveY + imgHeight/3) * -1);
+        context.translate((nextMoveX + imgWidth / 2) * -1, (nextMoveY + imgHeight / 3) * -1);
         context.drawImage(maskImg, nextMoveX, nextMoveY, imgWidth, imgHeight);
         context.restore();
     }
@@ -110,15 +127,33 @@ function saveImage() {
 
 function changeEffect(src) {
     maskImg = new Image();
+    maskImg.onload = function(){
+      maskFlag = true;
+    };
     maskImg.src = src;
+    document.getElementById('eff-control').style.display = 'flex';
 }
 function clearEffect() {
+    maskFlag = false;
     maskImg = null;
     uploadedImg = null;
+    moveX = 0;
+    moveY = 0;
+    isDragging = false;
+    prevX = 0;
+    prevY = 0;
+    prevCanvasWidth = -1;
+    document.getElementById('rotat').value = '0';
+    document.getElementById('resize').value = '0';
+    var event = document.createEvent('Event');
+    event.initEvent('input', true, true);
+    document.getElementById('rotat').dispatchEvent(event);
+    document.getElementById('resize').dispatchEvent(event);
+    document.getElementById('eff-control').style.display = 'none';
 }
 
 function uploadImg() {
-    var file = document.getElementById("getimage").files[0];
+    var file = document.getElementById("uploadImg").files[0];
     var reader = new FileReader();
     reader.onloadend = function() {
         uploadedImg = new Image();
@@ -129,19 +164,31 @@ function uploadImg() {
     }
 }
 
-document.getElementById("canvas").addEventListener("mousedown", function() {
+function uploadMask() {
+    var file = document.getElementById("uploadMask").files[0];
+    var reader = new FileReader();
+    reader.onloadend = function() {
+        changeEffect(reader.result);
+    };
+    if (file) {
+        reader.readAsDataURL(file);
+    }
+}
+
+
+function maskMoveStart() {
     isDragging = true;
     prevX=0;
     prevY=0;
-});
+}
 
-window.addEventListener("mouseup", function() {
+function maskMoveFinish() {
     isDragging = false;
     prevX=0;
     prevY=0;
-});
+}
 
-window.addEventListener("mousemove", function() {
+function maskMove() {
     if( isDragging === true && maskImg !== null) {
         if( prevX>0 || prevY>0) {
             moveX += event.pageX - prevX;
@@ -149,5 +196,90 @@ window.addEventListener("mousemove", function() {
         }
         prevX = event.pageX;
         prevY = event.pageY;
+        prevCanvasWidth = canvas.width;
     }
-});
+}
+
+function handleStart(evt) {
+    evt.preventDefault();
+    isDragging = true;
+    prevX=0;
+    prevY=0;
+}
+
+function handleMove(evt) {
+    evt.preventDefault();
+    var touches = evt.changedTouches;
+    if( isDragging === true && maskImg !== null) {
+        if( prevX>0 || prevY>0) {
+            moveX += touches[0].pageX - prevX;
+            moveY += touches[0].pageY  - prevY;
+        }
+        prevX = touches[0].pageX;
+        prevY = touches[0].pageY;
+        prevCanvasWidth = canvas.width;
+    }
+}
+
+function handleEnd(evt) {
+    evt.preventDefault();
+    console.log("touchend");
+    isDragging = false;
+    prevX=0;
+    prevY=0;
+}
+
+
+canvas.addEventListener("mousedown", maskMoveStart);
+canvas.addEventListener("touchstart", handleStart, false);
+
+window.addEventListener("mouseup", maskMoveFinish);
+canvas.addEventListener("touchend", handleEnd, false);
+
+window.addEventListener("mousemove", maskMove);
+canvas.addEventListener("touchmove", handleMove, false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var timeoutId = null;
+// var canvasHovered = false;
+//
+// canvas.addEventListener('mouseover',function() {
+//     timeoutId = window.setTimeout(function(){
+//         canvasHovered = true;
+//     }, 3000);
+// });
+//
+// canvas.addEventListener('click',function() {
+//     if (canvasHovered) {
+//         document.getElementById('uploadImg').click();
+//     }
+// });
+//
+// // Cancel your action if mouse moved out within 2 sec
+// canvas.addEventListener('mouseout',function() {
+//     window.clearTimeout(timeoutId);
+//     canvasHovered = false;
+// });
